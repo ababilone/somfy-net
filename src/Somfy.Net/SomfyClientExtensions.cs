b2c4ca6200fd92c7225ab9535c40cbc7f2d82436
A. Babilone <arnaud.babilone@gmail.com>
2020-09-25 02:14:02 +0200

    Fix IServiceCollection extension method

--- src/Somfy.Net/SomfyClientExtensions.cs
+++ src/Somfy.Net/SomfyClientExtensions.cs
@@ -6,9 +6,9 @@ namespace Somfy.Net
 {
   public static class SomfyClientExtensions
   {
-    public static IServiceCollection AddSomfyClient(this IServiceCollection serviceCollection, Func<Task<string>> getAccessTokenAsync)
+    public static IServiceCollection AddSomfyClient(this IServiceCollection serviceCollection, Func<IServiceProvider, Func<Task<string>>> getAccessTokenAsync)
     {
-      serviceCollection.AddSingleton(new SomfyAuthorizationContext(getAccessTokenAsync));
+      serviceCollection.AddSingleton(serviceProvider => new SomfyAuthorizationContext(getAccessTokenAsync(serviceProvider)));
       serviceCollection.AddSingleton<SomfyClient>();
       serviceCollection.AddHttpClient<SomfyClient>();
       return serviceCollection;
